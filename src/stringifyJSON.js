e998510666be0dc94b5e2b9714f41ddde39b5eb6
ChrisM13 <chrismosier13@gmail.com>
2016-08-28 18:49:24 -0700

    Final Draft of Recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,70 +3,71 @@
 
 // but you don't so you're going to write it from scratch:
 
-//how do you
-
- // var jsonString = "";
-//
- // // build recursive function
- // // takes obj properties, converts to strings
- // // takes property values, converts to strings
-//
-//
- // function toString(val){//
-
 var stringifyJSON = function(obj) {
+  //create an empty JSON string
   var myJSON = "";
-
   // check first for null / undefined / etc and return
   if (obj === null || obj === undefined){
-    myJSON += 'null'
+    //returns null string for null or undefined arguments
+    myJSON += 'null';
+
+  //tests value types of obj arguments: string, number, boolean, array, object
+    //if the test passes, add the obj to the myJSON string
+
   }else if (typeof obj === "string") {
     myJSON += ('"' + obj + '"')
   }else if (typeof obj === "number") {
     myJSON += (obj)
   }else if (typeof obj === "boolean") {
     myJSON += obj.toString();
-  }else if(Array.isArray(obj)){
+  }
+
+  // if obj is an array we have to apply stringify on each element
+
+  else if(Array.isArray(obj)){
+    // if the array is empty add the empty array to myJASON
     var stringArr = [];
     if (obj.length === 0){
       myJSON += stringArr;
+
+    // if the array isn't empty apply stringify to each element and push into stringArr array
     }else obj.forEach(function(index){
       stringArr.push(stringifyJSON(index));
-    })
+    });
+    //join the array using a comma - turns the array into a string
     stringArr.join(",");
+    // add string brackets around the string and add to myJSON
     myJSON += "[" + stringArr + "]";
   }
+
   else if(typeof obj === "object"){
-    var stringObj;
-    console.log("THIS IS HAS OWN PROP", obj.hasOwnProperty())
-    if (!obj.hasOwnProperty()){
-      myJSON += "{}";
-      console.log("This is the string OBJ", stringObj);
+
+    //this block of code is executed for nested objects
+    //objKeys creates new object and places source parameter as nested object key
+    var objKeys = Object.keys(obj);
+    //new array created to store key-value pairs, later to be added to stringified JSON object
+    var keyValueArray = new Array();
+
+    for (var i = 0; i < objKeys.length; i++) {
+        //these conditions are unstringifiable and will return an empty string object
+        if (typeof objKeys[i] === "function" || typeof obj[objKeys[i]] === "function" ||
+          typeof objKeys[i] === undefined || typeof obj[objKeys[i]] === undefined){
+          return "{}";
+        }else{
+        //this code will stringify the object keys and add in their values
+        var keyValueString = '"' + objKeys[i] + '":';
+        var objValue = obj[objKeys[i]];
+        //if the key-value pair is a string, cocat strings and push to key-value array
+        keyValueString = (typeof objValue == "string") ?
+            keyValueString = keyValueString + '"' + objValue + '"' :
+            keyValueString = keyValueString + stringifyJSON(objValue);
+        keyValueArray.push(keyValueString);
+        }
     }
-
-    else{
-      for (var p in obj){
-        console.log("THIS IS P DUDE", p);
-        stringObj[p] = stringifyJSON(p);
-      }
-    }
-
-
-
+    //join the key-value array with the stringified json object
+      myJSON += "{" + keyValueArray.join(",") + "}";
   }
-    // iterate through all the properties of the object
-        // check to see if this property is a string, number, etc
-        // if (typeof p === "string") {
-        //   myJSON += (p + ": " + obj[p] + ",")
-        // }else if (typeof p === "number") {
-        //   myJSON += (p + ": " + obj[p] + ",")
-        // }else if (typeof p === "object" && obj.length === undefined){
-        //   myJSON += stringifyJSON(obj[p]); // this is recursion!
-        // } else if (typeof p === "object" && obj.length > 0){
-        //   p.forEach(function(index){
-        //     stringifyJSON(index);
-        //   });
 
   return myJSON;
-  console.log("myJSON", myJSON, typeof myJSON);
 };
+
